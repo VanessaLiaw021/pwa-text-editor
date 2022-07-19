919363175de15f23b79e4a9cc1386c19ae6fd8f3
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-19 18:53:45 -0400

    added the functionality to getAll() data from database in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import { ContextExclusionPlugin } from 'webpack';
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -13,9 +14,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  
+  //Display message to know that the content is updated 
+  
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  //Display message to know we are getting all data from database
+  console.log("GET all data from the database");
+
+  //Create the connection to the database and the version we want to use 
+  const jateDB = await openDB('jate', 1);
+
+  //Create a new transaction and specify the database and data privileges 
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  //Open up object store 
+  const store = tx.objectStore('jate');
+
+  //Get all method to get all data in the database 
+  const request = store.getAll();
+  
+  //Confirm the request 
+  const result = await request;
+
+  //Log to know we successfully request the data 
+  console.log('result.value', result);
+
+  //Return the result 
+  return result;
+};
 
 initdb();
